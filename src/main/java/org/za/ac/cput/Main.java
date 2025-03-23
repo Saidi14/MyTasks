cf5a1b8be7a25ac3dc6138a51ca734a679db0f24
Samnkelo <221311564@mycput.ac.za>
2025-03-23 05:06:52 +0200

    testing

--- src/main/java/org/za/ac/cput/Main.java
+++ src/main/java/org/za/ac/cput/Main.java
@@ -3,5 +3,6 @@ package org.za.ac.cput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+	System.out.println("Checking if git push works");
     }
 }
