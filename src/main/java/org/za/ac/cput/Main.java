2793dd3081e4f84271a73157a932f4a850f38929
Samnkelo <221311564@mycput.ac.za>
2025-03-25 03:36:23 +0200

    Added Task, TaskFactory, Helper, and test code on Main

--- src/main/java/org/za/ac/cput/Main.java
+++ src/main/java/org/za/ac/cput/Main.java
@@ -1,8 +1,16 @@
 package org.za.ac.cput;
 
+import org.za.ac.cput.domain.Task;
+import org.za.ac.cput.factory.TaskFactory;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-	System.out.println("Checking if git push works");
+
+        Task task = TaskFactory.createTask("Clean shoes", "Clean the white shoes", "Low", "sh234k", "Not complete");
+
+        System.out.println(task.toString());
+
     }
 }
