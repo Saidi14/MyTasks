1eccb6bc4e0872d2d8e593af355549dc8471fbb2
Samnkelo <221311564@mycput.ac.za>
2025-03-27 01:28:09 +0200

    main - code testing

--- src/main/java/org/za/ac/cput/Main.java
+++ src/main/java/org/za/ac/cput/Main.java
@@ -7,8 +7,6 @@ import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -25,16 +23,34 @@ public class Main {
             int randomNumber = random.nextInt(1111)+1111;
             String taskId = Integer.toString(randomNumber);
 
-            System.out.println("\nEnter task title: ");
+            System.out.print("\nEnter task title: ");
             String title = scanner.nextLine();
 
             System.out.print("\nTask description: ");
             String description = scanner.nextLine();
 
-            System.out.print("\nTask priority (Low, Medium, or High: ");
-            String priority = scanner.nextLine();
+            //Testing the new way of selecting priority
+            System.out.print("\nTask priority (1.Low, 2.Medium, or 3.High: )");
+            System.out.print("\nEnter priority number (1, 2, or 3): ");
+            int priorityChoice = scanner.nextInt();
+            System.out.println(); // This line is not necessary, it's just for format on the console.
 
-            Task task = TaskFactory.createTask(title, description, priority, taskId, "Not complete");
+            Task.Priority priority;
+            switch (priorityChoice) {
+                case 1:
+                    priority = Task.Priority.Low;
+                    break;
+                case 2:
+                    priority = Task.Priority.Medium;
+                    break;
+                case 3:
+                    priority = Task.Priority.High;
+                    break;
+                default:
+                    priority = Task.Priority.Low;
+            }
+
+            Task task = TaskFactory.createTask(title, description, priority.toString(), taskId, String.valueOf(Task.Status.Not_Complete));
             System.out.println(task.toString());
         }
 
