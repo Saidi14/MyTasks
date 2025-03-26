cf6828994c1cebdb73d5cc3581ba15c0201d4264
Samnkelo <221311564@mycput.ac.za>
2025-03-27 01:31:10 +0200

    221311564

--- src/main/java/org/za/ac/cput/factory/TaskFactory.java
+++ src/main/java/org/za/ac/cput/factory/TaskFactory.java
@@ -6,13 +6,32 @@ import org.za.ac.cput.util.Helper;
 public class TaskFactory {
 
     public static Task createTask(String title, String description, String priority, String taskID, String status) {
-        if(Helper.isNullOrEmpty(title) || Helper.isNullOrEmpty(description) || Helper.isNullOrEmpty(priority) || Helper.isNullOrEmpty(taskID) || Helper.isNullOrEmpty(status));
+        if(Helper.isNullOrEmpty(title) || Helper.isNullOrEmpty(description) || Helper.isNullOrEmpty(priority) || Helper.isNullOrEmpty(taskID) || Helper.isNullOrEmpty(status)) {
+            throw new IllegalArgumentException("Check the arguments in 'createTask()'");
+        }
+
+        //For priority and status
+        Task.Priority taskPriority;
+        try {
+            taskPriority = Task.Priority.valueOf(priority);
+        }catch(IllegalArgumentException iae) {
+            throw new IllegalArgumentException("Invalid input: " + priority);
+        }
+
+        Task.Status taskStatus;
+        try{
+            taskStatus = Task.Status.valueOf(status);
+        }
+        catch(IllegalArgumentException iae) {
+            throw new IllegalArgumentException("Invalid input: " + status);
+        }
+
 
         return new Task.Builder().setTitle(title)
                     .setDescription(description)
-                    .setPriority(priority)
+                    .setPriority(taskPriority)
                     .setTaskID(taskID)
-                    .setStatus(status)
+                    .setStatus(taskStatus)
                     .build();
     }
 
